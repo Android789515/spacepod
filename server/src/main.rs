f11b2790f7e799815bca531f321a817d99d7ad4c
Android789515 <derflug@mailfence.com>
2025-08-01 03:02:06 -0500

    feat: serve built vite app

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                actix_files::Files::new("/", "../dist")
+                    .index_file("index.html")
+            )
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
