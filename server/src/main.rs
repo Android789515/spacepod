56805d0b08ecec29a465a8c350c602d427751e45
Android789515 <derflug@mailfence.com>
2025-08-09 04:31:53 -0500

    feat: add cors middleware to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,20 @@
 use std::io;
 
-use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer};
+use actix_cors::Cors;
+use actix_web::{get, web, App, HttpServer};
 
 #[get("/podcast-{url}")]
-async fn get_podcast(path: web::Path<String>) -> HttpResponse {
+async fn get_podcast(path: web::Path<String>) -> actix_web::Result<String> {
     let url = path.into_inner();
     let restructured_url = url.chars()
         .map(|char| if char == ':' { '/' } else { char })
         .collect::<String>();
 
-    let content = match reqwest::get(format!("https://{restructured_url}")).await {
+    match reqwest::get(format!("https://{restructured_url}")).await {
         Ok(response) => {
             match response.text().await {
                 Ok(content) => {
-                    content
+                    Ok(content)
                 },
                 Err(error) => {
                     panic!("Incorrect url {error}");
@@ -23,19 +24,20 @@ async fn get_podcast(path: web::Path<String>) -> HttpResponse {
         Err(error) => {
             panic!("Incorrect url {error}");
         },
-    };
-
-    HttpResponse::Ok()
-        .content_type(ContentType::xml())
-        .insert_header(("Access-Control-Allow-Origin", "https://www.space-pod.com"))
-        .insert_header(("Access-Control-Allow-Methods", "GET, OPTIONS"))
-        .body(content)
+    }
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("https://www.space-pod.com")
+            .allowed_origin("https://space-pod.com")
+            .allowed_methods(vec!["GET", "OPTIONS"])
+            .max_age(3600);
+
         App::new()
+            .wrap(cors)
             .service(get_podcast)
             .service(
                 actix_files::Files::new("/", "../dist")
