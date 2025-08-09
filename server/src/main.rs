a409a055c9317e85ae519f37fa15addaa81e0ebc
Android789515 <derflug@mailfence.com>
2025-08-09 04:06:42 -0500

    fix: cors error
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,19 @@
 use std::io;
 
-use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpServer};
 
 #[get("/podcast-{url}")]
-async fn get_podcast(path: web::Path<String>) -> HttpResponse {
+async fn get_podcast(path: web::Path<String>) -> actix_web::Result<String> {
     let url = path.into_inner();
     let restructured_url = url.chars()
         .map(|char| if char == ':' { '/' } else { char })
         .collect::<String>();
 
-    let content = match reqwest::get(format!("https://{restructured_url}")).await {
+    match reqwest::get(format!("https://{restructured_url}")).await {
         Ok(response) => {
             match response.text().await {
                 Ok(content) => {
-                    content
+                    Ok(content)
                 },
                 Err(error) => {
                     panic!("Incorrect url {error}");
@@ -23,13 +23,7 @@ async fn get_podcast(path: web::Path<String>) -> HttpResponse {
         Err(error) => {
             panic!("Incorrect url {error}");
         },
-    };
-
-    HttpResponse::Ok()
-        .content_type(ContentType::xml())
-        .insert_header(("Access-Control-Allow-Origin", "https://space-pod.com"))
-        .insert_header(("Access-Control-Allow-Methods", "GET, OPTIONS"))
-        .body(content)
+    }
 }
 
 #[actix_web::main]
